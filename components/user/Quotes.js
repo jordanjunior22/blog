9e452081dccf6cd742a97827870b8b807487ed02
jordanjunior22 <jordan.kheambo@final.edu.tr>
2025-07-12 14:07:26 +0100

    update

--- components/user/Quotes.js
+++ components/user/Quotes.js
@@ -5,8 +5,8 @@ function Quotes() {
   return (
     <div className='flex flex-col items-center bg-(--cta-color) gap-5 px-6 py-6 md:px-30 md:py-15 text-white'>
         <Quote/>
-        <p>"The universe is not only stranger than we imagine, but stranger than <br/> we can imagine. In poetry, we glimpse what logic cannot grasp."</p>
-        <h1>Alexander Matthews</h1>
+        <p>"Our first intentional act of the day upon waking up should be a profound smile of gratitude."</p>
+        <h1>Ni Timah</h1>
     </div>
   )
 }
