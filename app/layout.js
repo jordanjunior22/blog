e4617fb3b286f9c5ab353aecbcb550ac5d49c177
jordanjunior22 <jordan.kheambo@final.edu.tr>
2025-07-12 15:29:14 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { UserProvider } from '@/context/userContext';
 import { Nunito } from 'next/font/google';
 import Navigation from '@/components/user/Navigation';
 import Footer from '@/components/Footer';
-
+import Script from 'next/script'; // ⬅️ Import Script
 
 export const metadata = {
   title: 'Chest of Contemplation',
@@ -13,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <head>
+        {/* Add Google AdSense Script Here */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2286994703571724"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body>
         <UserProvider>
           <Navigation />
           {children}
